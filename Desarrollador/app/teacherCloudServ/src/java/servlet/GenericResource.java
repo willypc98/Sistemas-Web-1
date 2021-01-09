1d449c015d20bd22a3f14e51c56a02413a4f9a2a
David Recio Arnés <davidrecioarnes@gmail.com>
2021-01-09 18:16:36 +0100

    servlet hola, que dice hola mundo

--- Desarrollador/app/teacherCloudServ/src/java/servlet/GenericResource.java
+++ Desarrollador/app/teacherCloudServ/src/java/servlet/GenericResource.java
@@ -13,6 +13,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
 
 /**
  * REST Web Service
@@ -36,10 +37,12 @@ public class GenericResource {
      * @return an instance of java.lang.String
      */
     @GET
-    @Produces("hola")
+     @Path("hola")
+      @Produces(MediaType.TEXT_PLAIN)
     public String getXml() {
         //TODO return proper representation object
-       return " hio";
+        
+       return "hola mundo";
     }
 
     /**
