dbacabcf53b118f9472eaf59cc530474ba3caec7
David Recio Arnés <davidrecioarnes@gmail.com>
2021-01-16 22:23:24 +0100

    creado el clienete pero no hace nada

---
+++ Desarrollador/app/teacherCloudClienteWeb/src/java/Servlets/ServletClient.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+/**
+ * Jersey REST client generated for REST resource:GenericResource [Usuario]<br>
+ * USAGE:
+ * <pre>
+ *        ServletClient client = new ServletClient();
+ *        Object response = client.XXX(...);
+ *        // do whatever with response
+ *        client.close();
+ * </pre>
+ *
+ * @author david
+ */
+public class ServletClient {
+    
+}
