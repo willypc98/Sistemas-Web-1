7fe0ffee29d8823976d9752c29b2de5e4e532b7e
David Recio Arnés <davidrecioarnes@gmail.com>
2021-01-17 22:38:51 +0100

    mostrarCalificaciones se ve mejor

--- Desarrollador/app/teacherCloudClienteWeb/src/java/servlet/Inscribirse.java
+++ Desarrollador/app/teacherCloudClienteWeb/src/java/servlet/Inscribirse.java
@@ -11,13 +11,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import recursos.Clase;
 
 /**
  *
  * @author david
  */
-public class BuscarClase extends HttpServlet {
+public class Inscribirse extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -30,44 +29,21 @@ public class BuscarClase extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        Clase clase = new Clase();
         response.setContentType("text/html;charset=UTF-8");
-          String nombreClase = request.getParameter("nombreClase");
-        String respuesta = "";
-        String valor= "alloo";//te dice si falla o no
-
-       Modelo modelo = new Modelo();
-      
-    clase= modelo.buscarClase(nombreClase);
-     
-
-     
-//            ServletContext contexto = request.getServletContext();
-//            contexto.setInitParameter("id", id.toString());
-            respuesta = "Recibida, gracias por la respuesta";
-
         
-       
-  
-            response.setContentType("text/html;charset=UTF-8");
-            try (PrintWriter out = response.getWriter()) {
-                /* TODO output your page here. You may use following sample code. */
-                out.println("<!DOCTYPE html>");
-                out.println("<html>");
-                out.println("<head>");
-                out.println("<title>Servlet Login</title>");
-                out.println("</form>");
-                out.println("</head>");
-                out.println("<body>");
-                out.println("<h1>" + clase.getNombre()+ "</h1>");
-                out.println("<form method='post' action='/teacherCloudClienteWeb/Inscribirse' name='Inscribirse'>");
-                out.println("<button>Inscribirse</button>");
-                out.println("</body>");
-                out.println("</html>");
-            }
-
-        
-
+        Modelo modelo = new Modelo();
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Inscribirse</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Inscribirse at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
